6e265c39d95168a47a9d6969c0fd5e9ce2d0ac63
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2025-04-08 01:42:40 +0000

    added Js work

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Kyle Matthew Magnaye
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kyle Matthew Magnaye
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+
+const fahrenheitInput = document.getElementById("fahrenheit")
+const celsiusInput = document.getElementById("celsius")
+
+fahrenheitInput.addEventListener("input", () => {
+  const fahrenheit = parseFloat(fahrenheitInput.value)
+  if (!isNaN(fahrenheit)) {
+    const celsius = (((fahrenheit - 32) * 5) / 9).toFixed(2)
+    celsiusInput.value = celsius + " °C"
+  } else {
+    celsiusInput.value = ""
+  }
+})
